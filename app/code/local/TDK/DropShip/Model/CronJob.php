68b9a8fe8b14bdffb75799695baf84e274a9a837
Son Bui <sonbv00@gmail.com>
2018-03-05 03:51:00 +0700

    add cron job

---
+++ app/code/local/TDK/DropShip/Model/CronJob.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sonbv
+ * Date: 3/5/18
+ * Time: 3:46 AM
+ */
+
+class TDK_DropShip_Model_CronJob
+{
+
+    public function updateShipmentTrackingStatus()
+    {
+        echo 'sdf';
+        return $this;
+    }
+}
