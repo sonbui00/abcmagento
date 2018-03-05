99af076a026fc3f1034c04bffdf8ceccf982aaea
Son Bui <sonbv00@gmail.com>
2018-03-05 11:50:31 +0700

    cron job update data

--- app/code/local/TDK/DropShip/Model/CronJob.php
+++ app/code/local/TDK/DropShip/Model/CronJob.php
@@ -9,9 +9,57 @@
 class TDK_DropShip_Model_CronJob
 {
 
+    const STATUS_ERROR = 10;
+    const STATUS_DELIVERED = 20;
+
     public function updateShipmentTrackingStatus()
     {
-        echo 'sdf';
+        $tracks = $this->_getShipmentTracks();
+        foreach ($tracks as $track) {
+            $info = $track->getNumberDetail();
+            $description = '';
+            if ($info instanceof Mage_Shipping_Model_Tracking_Result_Status){
+                if ($info->getCarrier() === 'ups') {
+                    $description = $info->getStatus();
+                    if ($info->getStatus === 'DELIVERED') {
+                        $track->setStatus(static::STATUS_DELIVERED);
+                    }
+                } elseif ($info->getCarrier() === 'usps') {
+                    $description = $info->getTrackSummary();
+                    if (strpos($description,'was delivered') !== false) {
+                        $track->setStatus(static::STATUS_DELIVERED);
+                    }
+                }
+            } elseif ($info instanceof Mage_Shipping_Model_Tracking_Result_Error) {
+                $description = $info->getErrorMessage();
+            }
+            if ($description) {
+                $track->setDescription($description);
+                $track->save();
+            }
+            unset($info);
+        }
         return $this;
+    }
+
+    /**
+     * @return Mage_Sales_Model_Resource_Order_Shipment_Track_Collection
+     */
+    protected function _getShipmentTracks()
+    {
+        /* @var Mage_Sales_Model_Resource_Order_Shipment_Track_Collection $tracks */
+        $tracks = Mage::getResourceModel('sales/order_shipment_track_collection');
+        $tracks->getSelect()
+            ->joinRight(
+                array('dropshipOrder' => $tracks->getTable('tdk_dropship/supplier_order')),
+                'main_table.parent_id = dropshipOrder.shipment_id'
+            );
+        $tracks->addFieldToFilter('shipment_id',  array("notnull" => 1));
+        $tracks->addFieldToFilter('carrier_code', array("in" => array('usps', 'ups')));
+        $tracks->addFieldToFilter('status', array("null" => 1));
+
+        $createdAt = date('Y-m-d H:i:s', strtotime('-1 month')); //only push alerts generated in last hour
+        $tracks->addFieldToFilter('created_at',array('gteq' => $createdAt));
+        return $tracks;
     }
 }
