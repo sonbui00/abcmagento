0fc83354bdc6ac1f835123a251c4aa02e29ee1c6
Son Bui <sonbv00@gmail.com>
2018-03-08 00:35:42 +0700

    alway check store geoip

--- app/code/local/TDK/StoreRedirect/Model/Observer.php
+++ app/code/local/TDK/StoreRedirect/Model/Observer.php
@@ -12,20 +12,26 @@ class TDK_StoreRedirect_Model_Observer
     {
 //        echo(Mage::app()->getStore()->getCode());
         $cookie = Mage::getSingleton('core/cookie');
-        if ($cookie->get('geoip_processed') != 1) {
+        if (!$cookie->get('geoip_store')) {
 //            $ip = '72.229.28.185';// Mage::helper('core/http')->getRemoteAddr();
 //            $ip = '132.160.48.210';// Mage::helper('core/http')->getRemoteAddr();
             $ip = Mage::helper('core/http')->getRemoteAddr();
             /** @var MageWorx_GeoIP_Model_Geoip $geoIP */
             $geoIP = Mage::getSingleton('mageworx_geoip/geoip')->getLocation($ip);
             $geoIPRegion= $geoIP->getData('region');
-            if ('Hawaii' !== $geoIPRegion) {
-                $store = Mage::getModel('core/store')->load('mainland', 'code');
-                if ($store->getName() != Mage::app()->getStore()->getName()) {
-                    $event->getControllerAction()->getResponse()->setRedirect($store->getCurrentUrl(false));
-                }
+
+            if ('Hawaii' === $geoIPRegion) {
+                $geoIPStore = 'default';
+            } else {
+                $geoIPStore = 'mainland';
             }
-            $cookie->set('geoip_processed', 1, time() + 86400, '/');
+            $cookie->set('geoip_store', $geoIPStore, time() + 86400, '/');
+        } else {
+            $geoIPStore = $cookie->get('geoip_store');
+        }
+
+        if (Mage::app()->getStore()->getCode() !== $geoIPStore) {
+            $event->getControllerAction()->getResponse()->setRedirect(Mage::app()->getStore($geoIPStore)->getCurrentUrl(false));
         }
     }
 }
